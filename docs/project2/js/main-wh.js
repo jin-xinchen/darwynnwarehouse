e0774b484121dbeab75c7dc3a6bf28ab86de946a
tweebaajin <cxjinchen@gmail.com>
2022-02-23 22:56:56 -0500

    menu addEventListener querySelector

---
+++ docs/project2/js/main-wh.js
@@ -0,0 +1,14 @@
+document.getElementById("menu").addEventListener("click", function() {
+    document.getElementById("information").classList.toggle("close");
+    this.classList.toggle("close");
+});
+document.getElementById("information").addEventListener("click", function() {
+    this.classList.toggle("close");
+});
+const toggle_btn = document.querySelector('.js_toggle');
+const panel = document.querySelector('.panel');
+toggle_btn.onclick = function(e) {
+    // e.preventDefault();
+    panel.classList.toggle("panelClose"); //https://www.runoob.com/jsref/prop-element-classlist.html
+
+};
